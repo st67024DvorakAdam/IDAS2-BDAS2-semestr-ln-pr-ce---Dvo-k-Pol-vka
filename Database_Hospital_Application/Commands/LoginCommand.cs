8be6e487a89c97b8e7d748caee7306a2d299fc93
st67289 <davidecekcz@gmail.com>
2023-10-31 18:13:38 +0100

    update LoginWindow

--- Database_Hospital_Application/Commands/LoginCommand.cs
+++ Database_Hospital_Application/Commands/LoginCommand.cs
@@ -16,13 +16,30 @@ public class LoginCommand : BaseCommand
 
     public override void Execute(object? parameter)
     {
+        
         UserRepo userRepo = new UserRepo();
         var _username = _mainWindowViewModel.Username;
         var _password = _mainWindowViewModel.Password;
         User us = userRepo.LoginUser(_username, _password);
+
+        if (_username == null || _password == null) {
+            MessageBox.Show("Vyplňte prosím všechna pole! ", "Info", MessageBoxButton.OK, MessageBoxImage.Warning);
+            return; }
+
+        
+        if(us == null)
+        {
+            MessageBox.Show("Zadané přihlašovací údaje se neschodují!", "Info", MessageBoxButton.OK, MessageBoxImage.Error);
+            return;
+        }
+
         if(us.Name == _username && us.Password == _password) {
             MessageBox.Show("Execute metoda, username: " + _username + " password: " + _password, "Info", MessageBoxButton.OK, MessageBoxImage.Information);
             _mainWindowViewModel.OpenProfileWindow(us);
         }
+        
+            
+        
     }
 }
+
