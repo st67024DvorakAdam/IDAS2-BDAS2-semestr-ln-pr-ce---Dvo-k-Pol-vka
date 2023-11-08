9b31b8e32bcb4bc997ae5902e8dcf8f66ed9f0f3
Adam_Dvorak <st67024@upce.cz>
2023-11-08 16:56:20 +0100

    přidání, tlačítka pro edit foto, pak nastyluju

--- Database_Hospital_Application/ViewModels/ViewsVM/CurrUserVM.cs
+++ Database_Hospital_Application/ViewModels/ViewsVM/CurrUserVM.cs
@@ -8,6 +8,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media;
 using Database_Hospital_Application.Models.Enums;
 using Database_Hospital_Application.Models.Repositories;
+using Database_Hospital_Application.Commands;
+using System.Windows.Input;
+using System.Windows;
 
 namespace Database_Hospital_Application.ViewModels.ViewsVM
 {
@@ -30,6 +33,30 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             CurrentUser = currentUser;
            
         }
+
+        private ICommand _editPhotoCommand;
+        public ICommand EditPhotoCommand
+        {
+            get
+            {
+                if (_editPhotoCommand == null)
+                {
+                    _editPhotoCommand = new RelayCommand(EditPhoto);
+                }
+                return _editPhotoCommand;
+            }
+        }
+
+        private void EditPhoto(object parameter)
+        {
+            // Zde implementujte logiku pro editaci fotky, včetně zobrazení dialogu pro výběr souboru.
+            // Po úspěšném výběru souboru aktualizujte CurrentUser.Employee._foto.Image
+            // a provedete další operace, např. uložení do databáze.
+            MessageBox.Show("jsem tu");
+
+            //tohle pak dodělám
+        }
+
 
     }
 }
