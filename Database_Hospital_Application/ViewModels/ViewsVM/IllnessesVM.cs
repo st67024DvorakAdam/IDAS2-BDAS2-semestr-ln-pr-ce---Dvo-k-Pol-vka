e1e5b9a7ea5406640959720d848b4262dc2c5ca6
Adam_Dvorak <st67024@upce.cz>
2023-11-11 13:47:16 +0100

    nemoci filtr

--- Database_Hospital_Application/ViewModels/ViewsVM/IllnessesVM.cs
+++ Database_Hospital_Application/ViewModels/ViewsVM/IllnessesVM.cs
@@ -3,9 +3,11 @@ using Database_Hospital_Application.Models.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace Database_Hospital_Application.ViewModels.ViewsVM
 {
@@ -23,15 +25,49 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             }
         }
 
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
 
         public IllnessesVM()
         {
             LoadIllnessesAsync();
+            IllnessesView = CollectionViewSource.GetDefaultView(IllnessesList);
+            IllnessesView.Filter = IllnessesFilter;
         }
         private async Task LoadIllnessesAsync()
         {
             IllnessesRepo repo = new IllnessesRepo();
             IllnessesList = await repo.GetIllnessesAsync();
         }
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
+        ///
+
+        //FILTER/////////////////////////////////////////////////////////////////////
+
+        private string _searchText;
+        public ICollectionView IllnessesView { get; private set; }
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                _searchText = value;
+                OnPropertyChange(nameof(SearchText));
+                IllnessesView.Refresh();
+            }
+        }
+
+        private bool IllnessesFilter(object item)
+        {
+
+            if (string.IsNullOrWhiteSpace(_searchText)) return true;
+
+            var illness = item as Illness;
+            if (illness == null) return false;
+
+            return illness.Name.Contains(_searchText, StringComparison.OrdinalIgnoreCase)
+                || illness.Id.ToString().Contains(_searchText);
+        }
+        //FILTER/////////////////////////////////////////////////////////////////////
     }
 }
