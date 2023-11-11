d6b0acd26efd2659b5c9c5efd6b13071289f6758
Adam_Dvorak <st67024@upce.cz>
2023-11-11 14:14:31 +0100

    foto filtr

--- Database_Hospital_Application/ViewModels/ViewsVM/PhotoVM.cs
+++ Database_Hospital_Application/ViewModels/ViewsVM/PhotoVM.cs
@@ -3,9 +3,11 @@ using Database_Hospital_Application.Models.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace Database_Hospital_Application.ViewModels.ViewsVM
 {
@@ -19,9 +21,12 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             set { _photosList = value; OnPropertyChange(nameof(PhotosList)); }
         }
 
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
         public PhotoVM()
         {
             LoadPhotosAsync();
+            PhotosView = CollectionViewSource.GetDefaultView(PhotosList);
+            PhotosView.Filter = PhotosFilter;
         }
 
         private async Task LoadPhotosAsync()
@@ -29,5 +34,39 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             PhotosRepo repo = new PhotosRepo();
             PhotosList = await repo.GetAllPhotosAsync();
         }
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
+        ///
+
+        //FILTER/////////////////////////////////////////////////////////////////////
+
+        private string _searchText;
+        public ICollectionView PhotosView { get; private set; }
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                _searchText = value;
+                OnPropertyChange(nameof(SearchText));
+                PhotosView.Refresh();
+            }
+        }
+
+        private bool PhotosFilter(object item)
+        {
+
+            if (string.IsNullOrWhiteSpace(_searchText)) return true;
+
+            var foto = item as Foto;
+            if (foto == null) return false;
+
+            return foto.Id.ToString().Contains(_searchText)
+                || foto.Name.Contains(_searchText, StringComparison.OrdinalIgnoreCase)
+                || foto.Suffix.Contains(_searchText, StringComparison.OrdinalIgnoreCase)
+                || foto.DateOfUpload.ToString().Contains(_searchText, StringComparison.OrdinalIgnoreCase)
+                || foto.DateOfModification.ToString().Contains(_searchText, StringComparison.OrdinalIgnoreCase);
+        }
+        //FILTER/////////////////////////////////////////////////////////////////////
     }
 }
