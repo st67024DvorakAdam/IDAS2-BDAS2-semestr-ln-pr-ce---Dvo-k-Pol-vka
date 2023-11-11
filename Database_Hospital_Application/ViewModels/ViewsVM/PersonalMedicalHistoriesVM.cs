24e0ef83ed23c35c4e87e12f1d274f178c8fa42b
Adam_Dvorak <st67024@upce.cz>
2023-11-11 13:35:21 +0100

    osobní anamnézy filtr

--- Database_Hospital_Application/ViewModels/ViewsVM/PersonalMedicalHistoriesVM.cs
+++ Database_Hospital_Application/ViewModels/ViewsVM/PersonalMedicalHistoriesVM.cs
@@ -1,11 +1,14 @@
 ﻿using Database_Hospital_Application.Models.Entities;
+using Database_Hospital_Application.Models.Enums;
 using Database_Hospital_Application.Models.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace Database_Hospital_Application.ViewModels.ViewsVM
 {
@@ -19,9 +22,12 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             set { _personalMedicalHistoriesList = value; OnPropertyChange(nameof(PersonalMedicalHistoriesList)); }
         }
 
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
         public PersonalMedicalHistoriesVM()
         {
             LoadPersonalMedicalHistoriesAsync();
+            PersonalMedicalHistoriesView = CollectionViewSource.GetDefaultView(PersonalMedicalHistoriesList);
+            PersonalMedicalHistoriesView.Filter = PersonalMedicalHistoriesFilter;
         }
 
         private async Task LoadPersonalMedicalHistoriesAsync()
@@ -29,5 +35,40 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             PersonalMedicalHistoriesRepo repo = new PersonalMedicalHistoriesRepo();
             PersonalMedicalHistoriesList = await repo.GetAllPersonalMedicalHistoriesAsync();
         }
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
+        ///
+        //FILTER/////////////////////////////////////////////////////////////////////
+
+        // Hledaný řetezec v TextBoxu pro vyhledávání
+        private string _searchText;
+        public ICollectionView PersonalMedicalHistoriesView { get; private set; }
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                _searchText = value;
+                OnPropertyChange(nameof(SearchText));
+                PersonalMedicalHistoriesView.Refresh();
+            }
+        }
+
+        private bool PersonalMedicalHistoriesFilter(object item)
+        {
+
+            if (string.IsNullOrWhiteSpace(_searchText)) return true;
+
+            var personalMedicalHistory = item as PersonalMedicalHistory;
+            if (personalMedicalHistory == null) return false;
+
+
+
+            return personalMedicalHistory.Id.ToString().Contains(_searchText)
+                || personalMedicalHistory.BirthNumberOfPatient.ToString().Contains(_searchText)
+                || personalMedicalHistory.IdOfPatient.ToString().Contains(_searchText)
+                || personalMedicalHistory.Description.Contains(_searchText, StringComparison.OrdinalIgnoreCase);
+        }
+        //FILTER/////////////////////////////////////////////////////////////////////
     }
 }
