43cc7a1e0fbbc2aa44d35821d4ab71fe535160ce
Adam_Dvorak <st67024@upce.cz>
2023-11-11 13:29:25 +0100

    provedené výkony filtr

--- Database_Hospital_Application/ViewModels/ViewsVM/PerformedProceduresVM.cs
+++ Database_Hospital_Application/ViewModels/ViewsVM/PerformedProceduresVM.cs
@@ -1,11 +1,14 @@
 ﻿using Database_Hospital_Application.Models.Entities;
+using Database_Hospital_Application.Models.Enums;
 using Database_Hospital_Application.Models.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace Database_Hospital_Application.ViewModels.ViewsVM
 {
@@ -19,9 +22,12 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             set { _performedProceduresList = value; OnPropertyChange(nameof(PerformedProceduresList)); }
         }
 
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
         public PerformedProceduresVM()
         {
             LoadPerformedProceduresAsync();
+            PerformedProceduresView = CollectionViewSource.GetDefaultView(PerformedProceduresList);
+            PerformedProceduresView.Filter = PerformedProceduresFilter;
         }
 
         private async Task LoadPerformedProceduresAsync()
@@ -29,5 +35,43 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             PerformedProceduresRepo repo = new PerformedProceduresRepo();
             PerformedProceduresList = await repo.GetAllPerformedProceduresAsync();
         }
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
+        ///
+
+        //FILTER/////////////////////////////////////////////////////////////////////
+
+        // Hledaný řetezec v TextBoxu pro vyhledávání
+        private string _searchText;
+        public ICollectionView PerformedProceduresView { get; private set; }
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                _searchText = value;
+                OnPropertyChange(nameof(SearchText));
+                PerformedProceduresView.Refresh();
+            }
+        }
+
+        private bool PerformedProceduresFilter(object item)
+        {
+
+            if (string.IsNullOrWhiteSpace(_searchText)) return true;
+
+            var performedProcedure = item as PerformedProcedure;
+            if (performedProcedure == null) return false;
+
+
+
+            return performedProcedure.Name.Contains(_searchText, StringComparison.OrdinalIgnoreCase)
+                //|| performedProcedure.Id.ToString().Contains(_searchText)
+                //|| performedProcedure.IdOfPatient.ToString().Contains(_searchText)
+                || performedProcedure.BirthNumberOfPatient.ToString().Contains(_searchText)
+                || performedProcedure.Price.ToString().Contains(_searchText)
+                || performedProcedure.IsCoveredByInsurence.ToString().Contains(_searchText, StringComparison.OrdinalIgnoreCase);
+        }
+        //FILTER/////////////////////////////////////////////////////////////////////
     }
 }
