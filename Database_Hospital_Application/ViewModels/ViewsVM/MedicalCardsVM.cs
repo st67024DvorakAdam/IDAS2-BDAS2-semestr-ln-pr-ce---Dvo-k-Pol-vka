fcbcae0d8929368692c41f282b994de38f50ee00
Adam_Dvorak <st67024@upce.cz>
2023-11-11 13:42:52 +0100

    zdravotní karty - vyhledávání

--- Database_Hospital_Application/ViewModels/ViewsVM/MedicalCardsVM.cs
+++ Database_Hospital_Application/ViewModels/ViewsVM/MedicalCardsVM.cs
@@ -3,9 +3,11 @@ using Database_Hospital_Application.Models.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace Database_Hospital_Application.ViewModels.ViewsVM
 {
@@ -19,9 +21,13 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             set { _medicalCardsList = value; OnPropertyChange(nameof(MedicalCardsList)); }
         }
 
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
+
         public MedicalCardsVM()
         {
             LoadMedicalCardsAsync();
+            MedicalCardsView = CollectionViewSource.GetDefaultView(MedicalCardsList);
+            MedicalCardsView.Filter = MedicalCardsFilter;    
         }
 
         private async Task LoadMedicalCardsAsync()
@@ -29,5 +35,38 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             MedicalCardsRepo repo = new MedicalCardsRepo();
             MedicalCardsList = await repo.GetAllMedicalCardsAsync();
         }
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
+        ///
+
+        //FILTER/////////////////////////////////////////////////////////////////////
+
+        private string _searchText;
+        public ICollectionView MedicalCardsView { get; private set; }
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                _searchText = value;
+                OnPropertyChange(nameof(SearchText));
+                MedicalCardsView.Refresh();
+            }
+        }
+
+        private bool MedicalCardsFilter(object item)
+        {
+
+            if (string.IsNullOrWhiteSpace(_searchText)) return true;
+
+            var medicalCard = item as MedicalCard;
+            if (medicalCard == null) return false;
+
+            return medicalCard.IdOfPatient.ToString().Contains(_searchText)
+                || medicalCard.BirthNumberOfPatient.ToString().Contains(_searchText)
+                || medicalCard.StringVersionOfIllnesses.Contains(_searchText, StringComparison.OrdinalIgnoreCase);
+        }
+        //FILTER/////////////////////////////////////////////////////////////////////
+
     }
 }
