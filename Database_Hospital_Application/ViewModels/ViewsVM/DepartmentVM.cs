8f3f50e3202aaab7b4b78971a722d191a7df22d1
Adam_Dvorak <st67024@upce.cz>
2023-11-12 10:26:40 +0100

    oddělení filtr

--- Database_Hospital_Application/ViewModels/ViewsVM/DepartmentVM.cs
+++ Database_Hospital_Application/ViewModels/ViewsVM/DepartmentVM.cs
@@ -3,9 +3,11 @@ using Database_Hospital_Application.Models.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace Database_Hospital_Application.ViewModels.ViewsVM
 {
@@ -19,9 +21,12 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             set { _departmentsList = value; OnPropertyChange(nameof(DepartmentsList)); }
         }
 
+        ///KONSTRUKTOR ////////////////////////////////////////////////////////////////////////////////////////
         public DepartmentVM()
         {
             LoadDepartmentsAsync();
+            DepartmentsView = CollectionViewSource.GetDefaultView(DepartmentsList);
+            DepartmentsView.Filter = DepartmentsFilter;
         }
 
         private async Task LoadDepartmentsAsync()
@@ -29,5 +34,35 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM
             DepartmentRepo repo = new DepartmentRepo();
             DepartmentsList = await repo.GetAllDepartmentsAsync();
         }
+
+        //FILTER/////////////////////////////////////////////////////////////////////
+
+        private string _searchText;
+        public ICollectionView DepartmentsView { get; private set; }
+
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                _searchText = value;
+                OnPropertyChange(nameof(SearchText));
+                DepartmentsView.Refresh();
+            }
+        }
+
+        private bool DepartmentsFilter(object item)
+        {
+
+            if (string.IsNullOrWhiteSpace(_searchText)) return true;
+
+            var department = item as Department;
+            if (department == null) return false;
+
+            return department.Id.ToString().Contains(_searchText)
+                || department.IdOfHeadOfDepartment.ToString().Contains(_searchText)
+                || department.Name.Contains(_searchText, StringComparison.OrdinalIgnoreCase);
+        }
+        //FILTER/////////////////////////////////////////////////////////////////////
     }
 }
