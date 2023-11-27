7d67a052a229c026565d4904c6d7ffe25de0d84c
st67289 <davidecekcz@gmail.com>
2023-11-27 09:20:02 +0100

    actualillness VM

--- Database_Hospital_Application/ViewModels/ViewsVM/DoctorVM/PatientViewVM/AcutalIllnessVM.cs
+++ Database_Hospital_Application/ViewModels/ViewsVM/DoctorVM/PatientViewVM/AcutalIllnessVM.cs
@@ -2,6 +2,7 @@
 using Database_Hospital_Application.Views.Doctor.Patient;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,35 @@ namespace Database_Hospital_Application.ViewModels.ViewsVM.DoctorVM.PatientViewV
 {
     public class AcutalIllnessVM : BaseViewModel
     {
+
+        private ObservableCollection<DataActualIllness> _illnessList;
+
+        public ObservableCollection<DataActualIllness> IllnessList
+        {
+            get { return _illnessList; }
+            set
+            {
+                _illnessList = value;
+                OnPropertyChange(nameof(IllnessList));
+
+            }
+        }
         public AcutalIllnessVM() { }
         public AcutalIllnessVM(Patient currentPatient)
         {
+            LoadDataAsync(currentPatient.Id);
+        }
+
+        private void LoadDataAsync(int id)
+        {
 
         }
+    }
+
+    public class DataActualIllness
+    {
+        public string Illness { set; get; }
+        public string PrescriptedPills { set; get; }
+        public string Dosage { set; get; }
     }
 }
