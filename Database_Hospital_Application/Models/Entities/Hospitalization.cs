c25f199b365cd60a63178cad886c47cb1787bfc2
st67289 <davidecekcz@gmail.com>
2023-12-03 15:31:20 +0100

    hospitalizace

--- Database_Hospital_Application/Models/Entities/Hospitalization.cs
+++ Database_Hospital_Application/Models/Entities/Hospitalization.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Types;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace Database_Hospital_Application.Models.Entities
 {
     public class Hospitalization
     {
+        public int Id { get; set; }
+        public OracleDate DateIn { get; set; }
+        public OracleDate DateOut { get; set; }
+        public string Details { get; set; }
+        public int PatientId { get; set; }
+        public int DepartmentId { get; set; }
+
+        
+
+        public Hospitalization() { }
+
+        
     }
 }
