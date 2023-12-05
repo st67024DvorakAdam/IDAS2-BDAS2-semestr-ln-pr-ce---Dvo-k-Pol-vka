96fa159394e72297053d526e2aa3d05fa9ac407b
st67289 <davidecekcz@gmail.com>
2023-12-05 02:44:14 +0100

    zákroky VM, View

---
+++ Database_Hospital_Application/Views/Doctor/Patient/PerformedProceduresView.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Database_Hospital_Application.ViewModels.Dialogs.Edit;
+using Database_Hospital_Application.ViewModels.ViewsVM.DoctorVM.PatientViewVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Database_Hospital_Application.Views.Doctor.Patient
+{
+    /// <summary>
+    /// Interakční logika pro PerformedProceduresView.xaml
+    /// </summary>
+    public partial class PerformedProceduresView : UserControl
+    {
+        public PerformedProceduresView()
+        {
+            InitializeComponent();
+        }
+    }
+}
